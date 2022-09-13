3a766eae2698e5a97c7017d8847c86bb61c3fcc3
Pedro <pmbs.123@gmail.com>
2022-09-13 22:06:35 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import random
+
+TOKEN = ""
+
+description = """An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here."""
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="/", description=description, intents=intents)
+
+bot.run(TOKEN)
