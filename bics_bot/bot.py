e0d9e13919f43e4c73ab27450ddcf47a96292894
mattucak <github.ydd10@simplelogin.co>
2023-02-04 23:01:01 +0100

    load/unload feature scrapped
    the feature runs just fine, but does not do what we thought it did. no longer necessary.

--- bics_bot/bot.py
+++ bics_bot/bot.py
@@ -8,14 +8,12 @@ from server_ids import *
 
 load_dotenv()
 
-
 def get_intents():
     # - Loading intents
     intents = nextcord.Intents.default()
     intents.members = True
     intents.message_content = True
     return intents
-
 
 def load_extensions():
     for filename in os.listdir("./cogs/events"):
@@ -37,65 +35,6 @@ bot_description = """**BICS-THE-BOT** is a bot made for the BICS Student Server.
 bot = commands.Bot(
     command_prefix="!", description=bot_description, intents=get_intents()
 )
-
-
-@bot.command(hidden=True)
-async def load(ctx, extension):
-    user = ctx.author
-    user_roles = user.roles
-    role = nextcord.utils.get(ctx.guild.roles, name="Admin")
-    if role in user_roles:
-        if extension.find("cmd") == -1:
-            try:
-                bot.load_extension(f"cogs.events.{extension}")
-            except commands.ExtensionError as e:
-                pass
-        else:
-            try:
-                bot.load_extension(f"cogs.commands.{extension}")
-            except commands.ExtensionError as e:
-                pass
-    else:
-        await ctx.send("Insufficients Permissions", ephemeral=True)
-
-
-@bot.command(hidden=True)
-async def unload(ctx, extension):
-    user = ctx.author
-    user_roles = user.roles
-    role = nextcord.utils.get(ctx.guild.roles, name="Admin")
-    if role in user_roles:
-
-        if extension.find("cmd") == -1:
-            try:
-                bot.unload_extension(f"cogs.events.{extension}")
-            except commands.ExtensionError as e:
-                pass
-        else:
-            try:
-                bot.unload_extension(f"cogs.commands.{extension}")
-            except commands.ExtensionError as e:
-                pass
-    else:
-        await ctx.send("Insufficients Permissions", ephemeral=True)
-
-
-@bot.command(hidden=True, invoke_without_command=True)
-async def reload(ctx):
-    user = ctx.author
-    user_roles = user.roles
-    role = nextcord.utils.get(ctx.guild.roles, name="Admin")
-    if role in user_roles:
-        for filename in os.listdir("./cogs/events"):
-            if filename.endswith(".py") and filename != "__init__.py":
-                bot.reload_extension(f"cogs.events.{filename[:-3]}")
-
-        for filename in os.listdir("./cogs/commands"):
-            if filename.endswith(".py") and filename != "__init__.py":
-                bot.reload_extension(f"cogs.commands.{filename[:-3]}")
-    else:
-        await ctx.send("Insufficients Permissions", ephemeral=True)
-
 
 load_extensions()
 
