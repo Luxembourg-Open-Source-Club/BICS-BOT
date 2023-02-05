aee5958483fab3811428a55616b550c9f08ffd74
mattucak <github.ydd10@simplelogin.co>
2023-02-05 13:21:46 +0100

    separating different year courses in the embed

--- bics_bot/embeds/courses_embed.py
+++ bics_bot/embeds/courses_embed.py
@@ -7,11 +7,24 @@ sys.path.append("../")
 from server_ids import *
 
 class Courses_embed(Embed):
-    def __init__(self, enrolled_courses: list[str]):
+    def __init__(self, year1_courses: list[str], year2_courses: list[str], year3_courses: list[str]):
         title = "Enrolled Courses"
         super().__init__(colour=Colour.blue(), title=title)
-        for course in enrolled_courses:
+        for course in year1_courses:
             self.add_field(
-                name=course,
+                name="Year 1 Courses",
+                value=course,
+                inline=False
+            )
+        for course in year2_courses:
+            self.add_field(
+                name="Year 2 Courses",
+                value=course,
+                inline=False
+            )
+        for course in year3_courses:
+            self.add_field(
+                name="Year 3 Courses",
+                value=course,
                 inline=False
             )
