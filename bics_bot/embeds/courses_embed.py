d37dd53c486d98150143c80e98fb9d678dae3940
mattucak <github.ydd10@simplelogin.co>
2023-02-05 14:25:54 +0100

    mashed the lists into one list

--- bics_bot/embeds/courses_embed.py
+++ bics_bot/embeds/courses_embed.py
@@ -7,28 +7,28 @@ sys.path.append("../")
 from server_ids import *
 
 class Courses_embed(Embed):
-    def __init__(self, year1_courses: list[str], year2_courses: list[str], year3_courses: list[str]):
-        title = "Enrolled Courses"
-        super().__init__(colour=Colour.blue(), title=title)
-        if len(year1_courses)>0:
+    def __init__(self, courses: list[str]):
+        super().__init__(colour=Colour.blue(), title="Enrolled Courses")
+        self.add_field(name="ATTENTION", value="")
+        if len(courses[0])>0:
             self.add_field(name="Year 1 Courses", value="")
-            for course in year1_courses:
+            for course in courses[0]:
                 self.add_field(
                     name="",
                     value=course,
                     inline=False
                 )
-        if len(year2_courses)>0:
+        if len(courses[1])>0:
             self.add_field(name="Year 2 Courses", value="")
-            for course in year2_courses:
+            for course in courses[1]:
                 self.add_field(
                     name="",
                     value=course,
                     inline=False
                 )
-        if len(year3_courses)>0:
+        if len(courses[2])>0:
             self.add_field(name="Year 3 Courses", value="")
-            for course in year3_courses:
+            for course in courses[2]:
                 self.add_field(
                     name="",
                     value=course,
