7d02f2b6087d9ce0d350b5f442cfd9156f3a0e54
mattucak <github.ydd10@simplelogin.co>
2023-02-05 12:35:40 +0100

    added courses command
    moving the courses command into its own cog.  used for pre-selecting previously chosen courses

---
+++ bics_bot/cogs/commands/courses_cmd.py
@@ -0,0 +1,57 @@
+from nextcord.ext import commands
+from nextcord import SelectOption, application_command
+import nextcord
+from nextcord.ui import Select, View
+import sys, json
+from dropdowns.course_selection_dropdown import DropdownView
+
+sys.path.append("../../")
+
+BICS_GUILD_ID = 753535223798562886
+BICS_CLONE_GUILD_ID = 1014558774532509777
+
+with open("./data/discord_channels.json") as f:
+    text_channels = json.load(f)
+
+courses_list = []
+for year in text_channels["courses"].values():
+    for sem in year.values():
+        for course in sem:
+            courses_list.append(course["name"]) 
+
+class CoursesCmd(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @application_command.slash_command(
+        guild_ids=[BICS_GUILD_ID, BICS_CLONE_GUILD_ID],
+        description="Courses Selection.",
+    )
+    async def courses(self, interaction: nextcord.Interaction):
+        user = interaction.user
+        guild = interaction.guild
+
+        if len(user.roles) == 1:
+            # Means the user only has the default @everyone role, and nothing else.
+            await interaction.response.send_message(
+                f"You haven't yet introduced yourself! Make sure you use the **/intro** command first",
+                ephemeral=True,
+            )
+        else:
+            enrolled_courses = self.get_courses_enrolled(user, guild)
+            view = DropdownView(enrolled_courses=enrolled_courses)
+            await interaction.response.send_message(view=view)
+
+    def get_courses_enrolled(self, user:nextcord.Interaction.user, guild:nextcord.Guild):
+        enrolled = []
+        channels = guild.text_channels
+        for channel in channels:
+            if channel.name not in courses_list:
+                continue
+            if user in channel.members:
+                enrolled.append(channel.name)
+        return enrolled
+
+
+def setup(client):
+    client.add_cog(CoursesCmd(client))
