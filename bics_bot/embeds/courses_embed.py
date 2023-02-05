7137e087e04456bd2292fe374b21252ceaea30b5
mattucak <github.ydd10@simplelogin.co>
2023-02-05 14:31:50 +0100

    disclaimer message added

--- bics_bot/embeds/courses_embed.py
+++ bics_bot/embeds/courses_embed.py
@@ -9,7 +9,11 @@ from server_ids import *
 class Courses_embed(Embed):
     def __init__(self, courses: list[str]):
         super().__init__(colour=Colour.blue(), title="Enrolled Courses")
-        self.add_field(name="ATTENTION", value="")
+        self.add_field(name="ATTENTION", value="If you do not make any new choices " +
+            "in a the dropdown menus, the courses that \"seem\" like they are " +
+            "chosen in that menu will be reset. If you do not want to lose access to those " +
+            "courses; go in that dropdown menu, and select, then unselect " +
+            "a course, so that your chosen courses are updated.", inline=False)
         if len(courses[0])>0:
             self.add_field(name="Year 1 Courses", value="")
             for course in courses[0]:
