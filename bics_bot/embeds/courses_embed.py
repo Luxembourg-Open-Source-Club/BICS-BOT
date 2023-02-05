37e03cc63ee14ed686442189fd4e7c44e71dde2a
mattucak <github.ydd10@simplelogin.co>
2023-02-05 13:31:05 +0100

    visual fix

--- bics_bot/embeds/courses_embed.py
+++ bics_bot/embeds/courses_embed.py
@@ -10,21 +10,27 @@ class Courses_embed(Embed):
     def __init__(self, year1_courses: list[str], year2_courses: list[str], year3_courses: list[str]):
         title = "Enrolled Courses"
         super().__init__(colour=Colour.blue(), title=title)
-        for course in year1_courses:
-            self.add_field(
-                name="Year 1 Courses",
-                value=course,
-                inline=False
-            )
-        for course in year2_courses:
-            self.add_field(
-                name="Year 2 Courses",
-                value=course,
-                inline=False
-            )
-        for course in year3_courses:
-            self.add_field(
-                name="Year 3 Courses",
-                value=course,
-                inline=False
-            )
+        if len(year1_courses)>0:
+            self.add_field(name="Year 1 Courses", value="")
+            for course in year1_courses:
+                self.add_field(
+                    name="",
+                    value=course,
+                    inline=False
+                )
+        if len(year2_courses)>0:
+            self.add_field(name="Year 2 Courses", value="")
+            for course in year2_courses:
+                self.add_field(
+                    name="",
+                    value=course,
+                    inline=False
+                )
+        if len(year3_courses)>0:
+            self.add_field(name="Year 3 Courses", value="")
+            for course in year3_courses:
+                self.add_field(
+                    name="",
+                    value=course,
+                    inline=False
+                )
