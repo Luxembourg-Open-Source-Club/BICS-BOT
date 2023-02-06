5f1edc33286b20715e79a22094bca4444c913bb4
PedroS235 <pmbs.123@gmail.com>
2023-02-06 23:06:55 +0100

    [refactor] Improve system to retrieve text channels

--- bics_bot/cogs/commands/courses_cmd.py
+++ bics_bot/cogs/commands/courses_cmd.py
@@ -1,18 +1,10 @@
 from nextcord.ext import commands
 from nextcord import application_command
 import nextcord
-import json
+
 from bics_bot.dropdowns.course_selection_dropdown import DropdownView
 from bics_bot.config.server_ids import GUILD_BICS_ID, ROLE_ADMIN_ID, ROLE_BOT_DEV_ID
-
-with open("./bics_bot/data/discord_channels.json") as f:
-    text_channels = json.load(f)
-
-courses_list = []
-for year in text_channels["courses"].values():
-    for sem in year.values():
-        for course in sem:
-            courses_list.append(course["name"])
+from bics_bot.utils.channels_utils import retrieve_courses_text_channels_names
 
 
 class CoursesCmd(commands.Cog):
@@ -24,31 +16,29 @@ class CoursesCmd(commands.Cog):
         description="Courses Selection.",
     )
     async def courses(self, interaction: nextcord.Interaction):
-        if interaction.user.get_role(ROLE_ADMIN_ID) is None and interaction.user.get_role(ROLE_BOT_DEV_ID) is None:
-            await interaction.response.send_message(content="Sorry, this is not yet available for use :)", ephemeral=True)
-        else:
-            user = interaction.user
-            guild = interaction.guild
+        user = interaction.user
+        guild = interaction.guild
 
-            if len(user.roles) == 1:
-                # Means the user only has the default @everyone role, and nothing else.
-                await interaction.response.send_message(
-                    f"You haven't yet introduced yourself! Make sure you use the **/intro** command first",
-                    ephemeral=True,
-                )
+        if len(user.roles) == 1:
+            # Means the user only has the default @everyone role, and nothing else.
+            await interaction.response.send_message(
+                f"You haven't yet introduced yourself! Make sure you use the **/intro** command first",
+                ephemeral=True,
+            )
+        else:
+            if not user.get_role(ROLE_ADMIN_ID) and not user.get_role(ROLE_BOT_DEV_ID):
+                await interaction.response.send_message(content="Sorry, this is not yet available for use :)", ephemeral=True)
             else:
                 enrolled_courses = self.get_courses_enrolled(user, guild)
                 view = DropdownView(enrolled_courses=enrolled_courses)
                 disclaimer_message = """ATTENTION\n\nIf you do not make any new choices in a the dropdown menus, the courses that \"seem\" like they are chosen in that menu will be reset. If you do not want to lose access to those courses; go in that dropdown menu, and select, then unselect a course, so that your chosen courses are updated."""
                 await interaction.response.send_message(content=disclaimer_message, view=view, ephemeral=True)
 
-    def get_courses_enrolled(self, user: nextcord.Interaction.user, guild: nextcord.Guild):
+    def get_courses_enrolled(self, user: nextcord.Interaction.user, guild: nextcord.Guild) -> list[str]:
         enrolled = []
         channels = guild.text_channels
         for channel in channels:
-            if channel.name not in courses_list:
-                continue
-            if user in channel.members:
+            if channel.name in retrieve_courses_text_channels_names(guild) and user in channel.members:
                 enrolled.append(channel.name)
         return enrolled
 
