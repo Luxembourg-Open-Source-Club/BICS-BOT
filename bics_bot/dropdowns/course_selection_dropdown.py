434a78867801d2005bcc8f78295e77fe18eed58b
PedroS235 <pmbs.123@gmail.com>
2023-02-07 17:07:52 +0100

    [fix] Fix the course selection command to enroll/unroll the courses properly

--- bics_bot/dropdowns/course_selection_dropdown.py
+++ bics_bot/dropdowns/course_selection_dropdown.py
@@ -118,7 +118,7 @@ class DropdownItem3(nextcord.ui.Select):
 
 class DropdownView(nextcord.ui.View):
     def __init__(self, enrolled_courses: list[str]):
-        super().__init__(timeout=None)
+        super().__init__(timeout=5000)
         self.item1 = DropdownItem1(enrolled_courses)
         self.item2 = DropdownItem2(enrolled_courses)
         self.item3 = DropdownItem3(enrolled_courses)
@@ -126,6 +126,49 @@ class DropdownView(nextcord.ui.View):
         self.add_item(self.item2)
         self.add_item(self.item3)
         self.enrolled_courses = enrolled_courses
+
+    async def process_course(self, selected_courses, prev_selection, interaction):
+        print(f"Selected courses: {selected_courses}")
+        print(f"Previous selections: {prev_selection}")
+        if selected_courses == []:
+            # It either means that the  user wants to unroll from courses or
+            # no changes were made
+            if not prev_selection == []:
+                # User has removed every course selected -> unroll for every course
+                print("Courses to be processed")
+                print(prev_selection)
+                await self.give_course_permissions(prev_selection, interaction.user, interaction.guild)
+                print("-------------------------")
+        else:
+            courses_to_handle = []
+            # Only consider the courses that were changed
+            for course in selected_courses:
+                if course not in prev_selection:
+                    courses_to_handle.append(course)
+
+            # Consider the courses that were unchecked by the user -> unroll
+            for course in prev_selection:
+                if course not in selected_courses:
+                    courses_to_handle.append(course)
+
+            print("Courses to be processed")
+            print(courses_to_handle)
+            await self.give_course_permissions(courses_to_handle, interaction.user, interaction.guild)
+            print("-------------------------")
+
+    def separate_prev_selected_courses(self, prev_selection, courses_text_channels):
+        courses = {"year1": [], "year2": [], "year3": []}
+
+        for course in courses_text_channels["year1"]:
+            if course in prev_selection:
+                courses["year1"].append(course)
+        for course in courses_text_channels["year2"]:
+            if course in prev_selection:
+                courses["year2"].append(course)
+        for course in courses_text_channels["year3"]:
+            if course in prev_selection:
+                courses["year3"].append(course)
+        return courses
 
     @nextcord.ui.button(label="Confirm", style=nextcord.ButtonStyle.green, row=3)
     async def confirm_callback(
@@ -136,37 +179,24 @@ class DropdownView(nextcord.ui.View):
         year1_selected_courses = self.item1.values
         year2_selected_courses = self.item2.values
         year3_selected_courses = self.item3.values
+        prev_selection = self.separate_prev_selected_courses(
+            self.enrolled_courses, courses_text_channels)
 
-        if year1_selected_courses == []:
-            new = []
-            for c in courses_text_channels["year1"]:
-                if c in self.enrolled_courses:
-                    new.append(c)
-            await self.give_course_permissions(new, interaction.user, interaction.guild)
+        print("Starting processing")
+        print("Year 1")
+        await self.process_course(year1_selected_courses,
+                                  prev_selection["year1"], interaction)
+        print("-----------")
+        print("Year 2")
+        await self.process_course(year2_selected_courses,
+                                  prev_selection["year2"], interaction)
+        print("-----------")
+        print("Year 3")
+        await self.process_course(year3_selected_courses,
+                                  prev_selection["year3"], interaction)
 
-        else:
-            await self.give_course_permissions(year1_selected_courses, interaction.user, interaction.guild)
-
-        if year2_selected_courses == []:
-            new = []
-            for c in courses_text_channels["year2"]:
-                if c in self.enrolled_courses:
-                    new.append(c)
-            await self.give_course_permissions(new, interaction.user, interaction.guild)
-
-        else:
-            await self.give_course_permissions(year2_selected_courses, interaction.user, interaction.guild)
-
-        if year3_selected_courses == []:
-            new = []
-            for c in courses_text_channels["year3"]:
-                if c in self.enrolled_courses:
-                    new.append(c)
-            await self.give_course_permissions(new, interaction.user, interaction.guild)
-
-        else:
-            await self.give_course_permissions(year3_selected_courses, interaction.user, interaction.guild)
-
+        print("End process")
+        print("================================================================")
         embed = CoursesSelectionEmbed(
             [year1_selected_courses, year2_selected_courses, year3_selected_courses])
         # await self.give_course_permissions(courses, interaction.user, interaction.guild)
