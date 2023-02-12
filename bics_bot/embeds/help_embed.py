0694a5a00ea4863eea8dd6f76fdb289905932869
Pedro Soares <pmbs.123@gmail.com>
2023-02-12 15:06:51 +0100

    [refactor] Update all embeds to up to date information

--- bics_bot/embeds/help_embed.py
+++ bics_bot/embeds/help_embed.py
@@ -1,7 +1,7 @@
 from nextcord import Embed
 from nextcord import Colour
 
-from bics_bot.config.server_ids import CHANNEL_INTRO_ID
+from bics_bot.utils.file_manipulation import read_txt
 
 
 class HelpEmbed(Embed):
@@ -12,28 +12,4 @@ class HelpEmbed(Embed):
     def __init__(self):
         title = "Available bot commands"
         super().__init__(colour=Colour.blue(), title=title)
-        self.add_field(
-            name="/help",
-            value=" - Shows all the possible commands of the bot",
-            inline=False,
-        )
-        self.add_field(
-            name=f"/intro (only for new members)",
-            value=f""" - *Only works in <#{CHANNEL_INTRO_ID}>*\n - Enter this command in order to get a role and introduce yourself.""",
-            inline=False,
-        )
-        self.add_field(
-            name=f"/gamer",
-            value=f""" - Gives yourself the Gamer role which gives you access to a gamer channel where you can interact with other gamers in the server.""",
-            inline=False,
-        )
-        self.add_field(
-            name=f"/useful_links",
-            value=f""" - Shows some links that might be useful for you, such as the BSP enrolment form""",
-            inline=False,
-        )
-        self.add_field(
-            name=f"/harem",
-            value=f""" - Gives yourself the Harem role which gives you acess to the harem channel, where you can collect harem cards.""",
-            inline=False,
-        )
+        self.description = read_txt("./bics_bot/texts/help_embed.txt")
