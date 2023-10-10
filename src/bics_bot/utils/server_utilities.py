2b6836e63abeb33306cb2bfde82348ea73a63f31
Pedro Soares <pmbs.123@gmail.com>
2023-10-10 12:20:50 +0200

    feat(utils): create a function to retrieve server ids based on names

---
+++ src/bics_bot/utils/server_utilities.py
@@ -0,0 +1,66 @@
+import yaml
+from nextcord import Guild, Role
+
+
+def get_role_id_by_name(guild: Guild, name: str):
+    """
+    TODO: Missing docstrings
+    """
+    for role in guild.roles:
+        if role.name == name:
+            return role.id
+    return None
+
+
+def get_category_id_by_name(guild: Guild, name: str):
+    """
+    TODO: Missing docstrings
+    """
+    for category in guild.categories:
+        if category.name == name:
+            return category.id
+    return None
+
+
+def get_channel_id_by_name(guild: Guild, name: str):
+    """
+    TODO: Missing docstrings
+    """
+    for channel in guild.channels:
+        if channel.name == name:
+            return channel.id
+    return None
+
+
+def retrieve_server_ids(guild: Guild):
+    """
+    TODO: Missing docstrings
+    """
+    with open("./bics_bot/config/server_ids.yaml", "r") as f:
+        server_ids = yaml.safe_load(f)
+        config = {"roles": {}, "channels": {}, "categories": {}}
+
+        for role in server_ids["roles"]:
+            name = role.replace(" ", "-").lower()
+            role_id = get_role_id_by_name(guild, role)
+            if role_id is None:
+                print(f"Role {role} not found")
+                continue
+            config["roles"][name] = role_id
+
+        for category in server_ids["categories"]:
+            name = category.replace(" ", "-").lower()
+            category_id = get_category_id_by_name(guild, category)
+            if category_id is None:
+                print(f"Category {category} not found")
+                continue
+            config["categories"][name] = category_id
+
+        for channel in server_ids["channels"]:
+            name = channel.replace(" ", "-").lower()
+            channel_id = get_channel_id_by_name(guild, channel)
+            if channel_id is None:
+                print(f"Channel {channel} not found")
+                continue
+            config["channels"][name] = channel_id
+    return config
